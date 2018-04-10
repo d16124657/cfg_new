218f72664f09497c9f647a7bbfe539263e91da74
nana <d16124657@mydit.ie>
2018-04-10 18:40:17 +0100

    add changes in app

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import requests
 from  flask import Flask, render_template, request
 import follower_info_twitter
+
 app = Flask(__name__)
 
 @app.route("/") 
 def hello():
     return render_template("index.html")
+#def hello_someone(name):
+         return render_template("hello.html", name=name.title())
+@app.route("/signup", methods=["POST"])
+#def sign_up():
+	form_data = request.form
+	print (form_data["email"])
+	return "All OK"
 
-
-if __name__ == "__main__":
+#if __name__ == "__main__":
     app.run()
