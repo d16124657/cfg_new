6bc5e7315a0aaf4a3d24043dd55553f188573cf9
nana <d16124657@mydit.ie>
2018-04-10 18:47:53 +0100

    add app2

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import follower_info_twitter
 app = Flask(__name__)
 
 @app.route("/") 
-def hello():
+def index():
     return render_template("index.html")
 #def hello_someone(name):
          return render_template("hello.html", name=name.title())
-@app.route("/signup", methods=["POST"])
+#@app.route("/signup", methods=["POST"])
 #def sign_up():
 	form_data = request.form
 	print (form_data["email"])
